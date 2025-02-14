e9538f024f72d59c42372d8d73cfacbbd2a0df96
Leonardo Figueira Henriques <2120922@student.uma.pt>
2025-02-14 10:04:26 +0000

    Adicionado mais linhas para o greeting

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@ namespace HelloWorld
     class Program {
         static void Main() {
             Console.WriteLine("Im Hello Worlding but on GitHub!");
+            Console.WriteLine("Imagina ter a criatividade para adicionar algo de interessante.");
+            Console.WriteLine("Não podia ser eu.");
         }
     }
 }
